7853d37ffa567a752365f0b822af7175a1583681
ADELBYTES <mohnour.pto@gmail.com>
2023-10-15 17:37:38 +0000

    assigning values to variables

---
+++ resource-group.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "myTerraformGroup"{
+    location = "East US"
+    name = "myTerraformGroup"
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "resourceGroupName" {
+    type = string
+
+}
+
+variable "location" {
+    type = string
+
+
+}
+
+variable "tags" {
+type = map(any)
+
+}
